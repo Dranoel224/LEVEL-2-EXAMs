1f814972c36cef2c8c53e4c420d6e1386340974e
Dranoel224 <ringetbong@gmail.com>
2022-10-31 15:52:31 +0100

    EXAMS PRACTICALS QUESTION 1 AND 2

--- src/component/pages/shoppingBasket.js
+++ src/component/pages/shoppingBasket.js
@@ -1,5 +1,35 @@
-let items = ["biscuits", "apple", "Oranges", "banana", "pineapple"];
-let boughtItem = {item:items[index]};
+//let items = ["biscuits", "apple", "Oranges", "banana", "pineapple"];
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import cartReducer from './components/reducers/cartReducer';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Item1 from '../../images/item1.jpg'
+import Item2 from '../../images/item2.jpg'
+import Item3 from '../../images/item3.jpg'
+import Item4 from '../../images/item4.jpg'
+import Item5 from '../../images/item5.jpg'
+import Item6 from '../../images/item6.jpg'
+
+
+const store = createStore(cartReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
+
+const initState = {
+    items: [
+        {id:1,title:'Winter body', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:110,img:Item1},
+        {id:2,title:'Adidas', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:80,img: Item2},
+        {id:3,title:'Vans', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:120,img: Item3},
+        {id:4,title:'White', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:260,img:Item4},
+        {id:5,title:'Cropped-sho', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:160,img: Item5},
+        {id:6,title:'Blues', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:90,img: Item6}
+    ],
+    addedItems:[],
+    total: 0
+}
 let shoppingBasket = [];
 
 function SantaLuciaBasket(){
@@ -39,23 +69,3 @@ export default Navbar;
 
 
 
-import Item1 from '../../images/item1.jpg'
-import Item2 from '../../images/item2.jpg'
-import Item3 from '../../images/item3.jpg'
-import Item4 from '../../images/item4.jpg'
-import Item5 from '../../images/item5.jpg'
-import Item6 from '../../images/item6.jpg'
-
-
-const initState = {
-    items: [
-        {id:1,title:'Winter body', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:110,img:Item1},
-        {id:2,title:'Adidas', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:80,img: Item2},
-        {id:3,title:'Vans', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:120,img: Item3},
-        {id:4,title:'White', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:260,img:Item4},
-        {id:5,title:'Cropped-sho', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:160,img: Item5},
-        {id:6,title:'Blues', desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:90,img: Item6}
-    ],
-    addedItems:[],
-    total: 0
-}
